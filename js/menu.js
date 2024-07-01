4650a4c536901512832438e73b789295173e54c4
KatherinMoran <morankatherin047@gmail.com>
2024-06-30 23:29:36 -0300

    trabajo practico

---
+++ js/menu.js
@@ -0,0 +1,29 @@
+    const menu = [       
+        { numero: '1', nombre: 'Cortado', image: "https://cdn.pixabay.com/photo/2020/08/17/14/37/coffee-5495609_1280.jpg" }, 
+        { numero: '2', nombre: 'Americano', image: "https://cdn.pixabay.com/photo/2015/07/13/14/58/coffee-843278_960_720.jpg" }, 
+        { numero: '3', nombre: 'Espresso', image: "https://cdn.pixabay.com/photo/2019/01/02/18/40/coffee-3909399_1280.jpg" },
+        { numero: '4', nombre: 'Latte', image: "https://cdn.pixabay.com/photo/2020/10/05/06/12/drink-5628282_960_720.jpg" },
+        { numero: '5', nombre: 'Mocha', image: "https://cdn.pixabay.com/photo/2015/11/25/21/54/cafe-mocha-1063007_1280.jpg" },
+        { numero: '6', nombre: 'Capuchino', image: "https://cdn.pixabay.com/photo/2018/10/06/11/22/coffee-3727673_1280.jpg" },
+        { numero: '7', nombre: 'Macchiato', image: "https://cdn.pixabay.com/photo/2015/02/19/23/28/coffee-642836_1280.jpg" },
+        { numero: '8', nombre: 'Affogato', image: "https://cdn.pixabay.com/photo/2015/02/04/09/06/affogato-623516_960_720.jpg" },
+        { numero: '9', nombre: 'Frappé', image: "https://cdn.pixabay.com/photo/2020/03/07/05/18/beverage-4908765_1280.jpg" },
+        { numero: '10', nombre: 'Dalgona', image: "https://cdn.pixabay.com/photo/2022/11/07/04/58/dalgona-coffee-7575608_960_720.jpg" },
+        { numero: '11', nombre: 'Café con Crema', image: "https://cdn.pixabay.com/photo/2016/08/09/09/59/coffee-1580232_1280.jpg" },
+        { numero: '12', nombre: 'Caramel Macchiato', image: "https://images.pexels.com/photos/15086187/pexels-photo-15086187/free-photo-of-cafeina-cafe-beber-bebida.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+        { numero: '13', nombre: 'Especial Mascota', image: "https://cdn.pixabay.com/photo/2021/01/31/06/36/coffee-5965985_960_720.jpg" },
+        { numero: '14', nombre: 'Batido de Chocolate', image: "https://cdn.pixabay.com/photo/2015/11/23/11/54/chocolate-smoothie-1058191_1280.jpg" },
+        { numero: '15', nombre: 'Batido de Kiwi', image: "https://cdn.pixabay.com/photo/2017/07/25/17/28/smoothie-2538962_960_720.jpg" },
+        { numero: '16', nombre: 'Cookies', image: "https://cdn.pixabay.com/photo/2019/06/01/21/02/cookie-4245030_960_720.jpg" },
+        { numero: '17', nombre: 'Macarons', image: "https://cdn.pixabay.com/photo/2019/03/13/21/16/cookies-4053771_1280.jpg"},
+        { numero: '18', nombre: 'Tostadas x6', image: "https://cdn.pixabay.com/photo/2018/11/25/02/01/breakfast-3836731_1280.jpg" },
+        { numero: '19', nombre: 'Rollos de Canela', image: "https://cdn.pixabay.com/photo/2016/05/26/16/27/cinnamon-rolls-1417494_1280.jpg" },
+        { numero: '20', nombre: 'Torta de Zanahoria', image: "https://cdn.pixabay.com/photo/2018/05/20/15/45/cake-3416079_960_720.jpg" },
+        { numero: '21', nombre: 'Donas x3', image: "https://cdn.pixabay.com/photo/2021/02/05/14/24/food-5984722_1280.jpg"},
+        { numero: '22', nombre: 'Torta de Chocolate', image: "https://cdn.pixabay.com/photo/2018/08/01/12/04/cake-3577073_960_720.jpg" },
+        { numero: '23', nombre: 'Torta de Frutilla', image: "https://cdn.pixabay.com/photo/2016/02/26/18/39/sweets-1224506_960_720.jpg" },
+        { numero: '24', nombre: 'Red Velvet', image: "https://cdn.pixabay.com/photo/2016/02/29/00/19/cake-1227842_1280.jpg" },
+        { numero: '25', nombre: 'Brownie', image: "https://cdn.pixabay.com/photo/2017/07/31/19/16/chocolate-2560177_1280.jpg" }
+    ];
+
+
